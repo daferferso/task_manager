ff2b0c6af1d862b94f75d841e5793e1e4d47a5f6
daferferso <daferferso@gmail.com>
2024-10-22 21:58:31 -0400

    add api controller - Implement basic CRUD endpoints about tasks

--- src/main_api.py
+++ src/main_api.py
@@ -1,6 +1,11 @@
 import uvicorn
-from fastapi import FastAPI
+from fastapi import FastAPI, APIRouter
 from pydantic import BaseModel
+from task.application.task_service import TaskService
+from task.infrastructure.task_repository_json import TaskRepositoryJson
+
+task_repository = TaskRepositoryJson("./data.json")
+task_service = TaskService(task_repository)
 
 
 class Task(BaseModel):
@@ -15,11 +20,32 @@ class TaskResponse(BaseModel):
 
 
 app = FastAPI()
-
-@app.get("/")
-def get_root():
-    return "hello :s"
+router = APIRouter(prefix="/tareas")
 
 
+@router.post("/", status_code=202)
+def add_task(task: Task):
+    task_service.add_task(task_name=task.name, task_description=task.description)
+    return {"message": "Task added successfull"}
+
+
+@router.delete("/{task_id}")
+def delete_task(task_id: str):
+    task_service.delete_task(task_id=task_id)
+    return {"message": "Task deleted successfull"}
+
+
+@router.put("/{task_id}")
+def mark_as_completed_task(task_id: str):
+    task_service.mark_task_as_completed(task_id=task_id)
+    return {"message": "Task completed successfull"}
+
+
+@router.get("/")
+def get_all_pending_tasks():
+    return task_service.get_pending_tasks()
+
+
+app.include_router(router)
 if __name__ == "__main__":
     uvicorn.run("main_api:app", host="127.0.0.1", port=8000, reload=True)
