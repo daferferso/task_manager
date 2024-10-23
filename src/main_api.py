f386947373ddba8eb4773532def196099418d7c7
daferferso <daferferso@gmail.com>
2024-10-22 22:13:21 -0400

    add api controller - Implement error handler in routes

--- src/main_api.py
+++ src/main_api.py
@@ -1,8 +1,9 @@
 import uvicorn
-from fastapi import FastAPI, APIRouter
+from fastapi import FastAPI, APIRouter, HTTPException
 from pydantic import BaseModel
 from task.application.task_service import TaskService
 from task.infrastructure.task_repository_json import TaskRepositoryJson
+from task.domain.task_errors import TaskAlReadyCompletedError, TaskNotFound
 
 task_repository = TaskRepositoryJson("./data.json")
 task_service = TaskService(task_repository)
@@ -13,10 +14,11 @@ class Task(BaseModel):
     description: str
 
 
-class TaskResponse(BaseModel):
+class TaskResponseModel(BaseModel):
     id: str
     name: str
     description: str
+    completed: bool
 
 
 app = FastAPI()
@@ -31,14 +33,22 @@ def add_task(task: Task):
 
 @router.delete("/{task_id}")
 def delete_task(task_id: str):
-    task_service.delete_task(task_id=task_id)
-    return {"message": "Task deleted successfull"}
+    try:
+        task_service.delete_task(task_id=task_id)
+        return {"message": "Task deleted successfull"}
+    except TaskNotFound:
+        raise HTTPException(status_code=404, detail="Task not found")
 
 
 @router.put("/{task_id}")
 def mark_as_completed_task(task_id: str):
-    task_service.mark_task_as_completed(task_id=task_id)
-    return {"message": "Task completed successfull"}
+    try:
+        task_service.mark_task_as_completed(task_id=task_id)
+        return {"message": "Task completed successfull"}
+    except TaskAlReadyCompletedError:
+        raise HTTPException(status_code=409, detail="Task already completed")
+    except TaskNotFound:
+        raise HTTPException(status_code=404, detail="Task not found")
 
 
 @router.get("/")
@@ -46,11 +56,14 @@ def get_all_pending_tasks():
     return task_service.get_pending_tasks()
 
 
-@router.get("/{task_id}")
+@router.get("/{task_id}", response_model=TaskResponseModel)
 def get_task(task_id):
-    return task_service.get_task(task_id)
-
+    try:
+        return task_service.get_task(task_id)
+    except TaskNotFound:
+        raise HTTPException(status_code=404, detail="Task not found")
 
 app.include_router(router)
+
 if __name__ == "__main__":
     uvicorn.run("main_api:app", host="127.0.0.1", port=8000, reload=True)
